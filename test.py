9d34040fdad875f50abd38ceebe94605f0df56aa
James Shen <james.shen@guidebee.com>
2020-05-01 08:46:02 +0800

    remove comments

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ import gym
 
 import guessing_number
 
-
 class RandomAgent(object):
     """The world's simplest agent!"""
 
@@ -14,7 +13,7 @@ class RandomAgent(object):
 
 
 class BetterRandomAgent(object):
-    """The world's simplest agent!"""
+    """The world's 2nd simplest agent!"""
 
     def __init__(self, action_space):
         self.action_space = action_space
@@ -56,11 +55,6 @@ if __name__ == '__main__':
                 total_reward += reward
                 total_guesses += int(info["guesses"])
                 break
-            # Note there's no env.render() here. But the environment still can open window and
-            # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
-            # Video is not recorded every episode, see capped_cubic_video_schedule for details.
-
-    # Close the env and write monitor result info to disk
 
     print(f'Total better random reward {total_reward}, average guess {round(total_guesses / 100, 1)}')
 
@@ -82,9 +76,6 @@ if __name__ == '__main__':
                 total_reward += reward
                 total_guesses += int(info["guesses"])
                 break
-            # Note there's no env.render() here. But the environment still can open window and
-            # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
-            # Video is not recorded every episode, see capped_cubic_video_schedule for details.
 
     # Close the env and write monitor result info to disk
     env.close()
